885bee268d68471e5c11b210fc17a677dd9cc960
Andrey S <pesopi@mail.ru>
2022-04-27 19:25:39 +0300

    Мелкие доработки

--- src/view/add-point-view.js
+++ src/view/add-point-view.js
@@ -1,8 +1,10 @@
 import {
   createElement,
 } from '../render.js';
+
 const createAddPointTemplate = () => (
-  `<form class="event event--edit" action="#" method="post">
+  `<li class="trip-events__item">
+  <form class="event event--edit" action="#" method="post">
 <header class="event__header">
   <div class="event__type-wrapper">
     <label class="event__type  event__type-btn" for="event-type-toggle-1">
@@ -161,7 +163,8 @@ const createAddPointTemplate = () => (
     </div>
   </section>
 </section>
-</form>`
+</form>
+</li>`
 );
 
 export default class AddPointView {
--- src/view/edit-point-view.js
+++ src/view/edit-point-view.js
@@ -3,7 +3,8 @@ import {
 } from '../render.js';
 
 const createEditPointTemplate = () => (
-  `<form class="event event--edit" action="#" method="post">
+  `<li class="trip-events__item">
+  <form class="event event--edit" action="#" method="post">
   <header class="event__header">
     <div class="event__type-wrapper">
       <label class="event__type  event__type-btn" for="event-type-toggle-1">
@@ -155,7 +156,8 @@ const createEditPointTemplate = () => (
       <p class="event__destination-description">Chamonix-Mont-Blanc (usually shortened to Chamonix) is a resort area near the junction of France, Switzerland and Italy. At the base of Mont Blanc, the highest summit in the Alps, it's renowned for its skiing.</p>
     </section>
   </section>
-</form>`
+</form>
+</li>`
 );
 
 export default class EditPointView {
